d5ee3ad2ca32cca2c644b19f605f5a299d2ce648
Mugil-Gokul <gokult@mugil.io>
2025-09-06 20:27:37 +0530

    Backend Done

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import ContactRoutes from './routes/ContactRoutes.js';
+import {mongoose} from "mongoose";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config();
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}));
+
+//database connection
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log('Database Connected Successfully'))
+.catch((err)=> console.log('Database not Connected' , err))
+
+
+const PORT=process.env.PORT;
+
+app.use("/api/contact", ContactRoutes);
+
+
+app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
